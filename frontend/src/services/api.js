5fc9f81ddd0dcbc1fac044cc7d1791f04919c129
madidigitalmkt3397-dev <madi.digitalmkt3397@gmail.com>
2026-09-18 21:54:01 -0300

    api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://radar-virais-1.onrender.com/',
 });
 
 export const pesquisarRadar = async (dadosFiltro) => {
